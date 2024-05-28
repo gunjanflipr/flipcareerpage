ab91679a33d44a8ae9505fb3c540308b710e1b3f
Gunjan Jain <jain10gunjan@gmail.com>
2024-05-28 17:35:35 +0530

    Created Header Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client"
 
 import FeatureSection from "@/components/Featuresection";
+import Header from "@/components/Header";
 import Openposs from "@/components/OpenPositions";
 import Slider from "@/components/Slider";
  
 export default function Home() {
   return (
     <main>
-      <Openposs/>
+      <Header/>
     </main>
   );
 }
